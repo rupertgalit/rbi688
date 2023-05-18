b8cdb8250a31a8ff74b5636d64b372e115720abb
John Matthew Crisostomo <jcrisostomo@netglobalsolutions.net>
2023-05-18 02:42:19 +0000

    Sucuri Audits

--- wp-content/uploads/sucuri/sucuri-failedlogins.php
+++ wp-content/uploads/sucuri/sucuri-failedlogins.php
@@ -1,11 +1,16 @@
 <?php exit(0); ?>
-{"user_login":"rhodium","attempt_time":1684313653,"remote_addr":"162.214.107.60","user_agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko\/20100101 Firefox\/43.0"}
-{"user_login":"rhodium","attempt_time":1684313660,"remote_addr":"93.113.96.210","user_agent":"Mozilla\/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/91.0.4472.114 Safari\/537.36"}
-{"user_login":"rhodium","attempt_time":1684313660,"remote_addr":"68.178.227.210","user_agent":"Mozilla\/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/60.0.3112.90 Safari\/537.36"}
-{"user_login":"rhodium","attempt_time":1684313664,"remote_addr":"81.88.52.174","user_agent":"Mozilla\/5.0 (Linux; Android 10; LM-Q710(FGN)) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/91.0.4472.114 Mobile Safari\/537.36"}
-{"user_login":"rhodium","attempt_time":1684313676,"remote_addr":"162.214.107.60","user_agent":"Mozilla\/5.0 (iPod; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit\/605.1.15 (KHTML, like Gecko) CriOS\/91.0.4472.80 Mobile\/15E148 Safari\/604.1"}
-{"user_login":"rhodium","attempt_time":1684313677,"remote_addr":"217.21.73.201","user_agent":"Mozilla\/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko\/20100101 Firefox\/54.0"}
-{"user_login":"rhodium","attempt_time":1684313679,"remote_addr":"91.195.62.2","user_agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko\/20100101 Firefox\/18.0"}
-{"user_login":"rhodium","attempt_time":1684313681,"remote_addr":"35.185.76.79","user_agent":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/74.0.3729.169 Safari\/537.36"}
-{"user_login":"rhodium","attempt_time":1684313686,"remote_addr":"87.106.229.164","user_agent":"Mozilla\/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/60.0.3112.90 Safari\/537.36"}
-{"user_login":"rhodium","attempt_time":1684313905,"remote_addr":"103.130.218.178","user_agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/36.0.1985.125 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377353,"remote_addr":"202.134.19.110","user_agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/47.0.2526.111 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377429,"remote_addr":"199.167.147.66","user_agent":"Mozilla\/5.0 (Windows NT 5.1) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/46.0.2490.71 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377430,"remote_addr":"43.154.17.104","user_agent":"Mozilla\/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/91.0.4472.114 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377438,"remote_addr":"103.142.26.178","user_agent":"Mozilla\/5.0 (X11; Linux x86_64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/44.0.2403.157 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377439,"remote_addr":"176.9.5.81","user_agent":"Mozilla\/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit\/605.1.15 (KHTML, like Gecko) CriOS\/91.0.4472.80 Mobile\/15E148 Safari\/604.1"}
+{"user_login":"rhodium","attempt_time":1684377440,"remote_addr":"5.39.4.40","user_agent":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/80.0.3987.163 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377443,"remote_addr":"91.195.62.2","user_agent":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/91.0.4472.114 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377448,"remote_addr":"217.21.73.201","user_agent":"Mozilla\/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/79.0.3945.88 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377453,"remote_addr":"51.77.231.194","user_agent":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko\/20100101 Firefox\/84.0"}
+{"user_login":"rhodium","attempt_time":1684377462,"remote_addr":"50.62.177.120","user_agent":"Mozilla\/5.0 (Linux; Android 10) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/91.0.4472.114 Mobile Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377463,"remote_addr":"148.66.145.144","user_agent":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/90.0.4430.212 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377465,"remote_addr":"89.107.186.233","user_agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko\/20100101 Firefox\/42.0"}
+{"user_login":"rhodium","attempt_time":1684377466,"remote_addr":"176.9.5.81","user_agent":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/78.0.3904.108 Safari\/537.36"}
+{"user_login":"rhodium","attempt_time":1684377469,"remote_addr":"50.62.177.120","user_agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko\/20100101 Firefox\/47.0"}
+{"user_login":"rhodium","attempt_time":1684377477,"remote_addr":"176.9.5.81","user_agent":"Mozilla\/5.0 (Linux; Android 10; LM-Q710(FGN)) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/91.0.4472.114 Mobile Safari\/537.36"}
